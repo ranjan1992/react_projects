e51919a8fbce2fd36ad4a530edf264446ca94197
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2024-11-15 00:01:02 +0530

    Build a Stats Component

--- p1_ageteller/src/App.js
+++ p1_ageteller/src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { Form, FormControl, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import AgeTeller from "./components/AgeTeller";
 import "./App.css";
 
 const App = () => {
+  const [newDate, setNewDate] = useState("");
+  const [birthday, setBirthDay] = useState("1992-06-21");
+  const changeBirthDay = () => {
+    console.log(newDate);
+    setBirthDay(newDate);
+  };
   return (
-    <div className="App">
-      <Form inline>
-        <h2>Input your Birthday!</h2>
-        <FormControl type="date"></FormControl>
-        {"  "}
-        <Button>Submit</Button>
-      </Form>
-    </div>
+    <>
+      <div className="App">
+        <Form>
+          <h2>Input your Birthday!</h2>
+          <Form.Control
+            type="date"
+            onChange={(event) => setNewDate(event.target.value)}
+          ></Form.Control>
+          {"  "}
+          <Button onClick={() => changeBirthDay()}>Submit</Button>
+        </Form>
+      </div>
+      <AgeTeller date={birthday} />
+    </>
   );
 };
 
---
+++ p1_ageteller/src/components/AgeTeller.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AgeTeller = ({ date }) => {
+  const timeSince = (date) => {
+    let today = new Date().getTime();
+    let other_date = new Date(date).getTime();
+    let difference = Math.abs(today - other_date);
+
+    let days = Math.floor(difference / (1000 * 3600 * 24));
+    let years = Math.floor(days / 365);
+    days -= years * 365;
+    let months = Math.floor(days / 31);
+    days -= months * 31;
+    return `${years} years ${months} months, and ${days} days`;
+  };
+  return (
+    <div className="Ageteller">
+      <h3>{date}</h3>
+      <p>Congrats on {timeSince(date)}!</p>
+    </div>
+  );
+};
+
+export default AgeTeller;
