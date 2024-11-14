9ac52a669f2c7d1ab581906b99dd9e747ee9d6c1
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2024-11-14 23:05:06 +0530

    Crafting a Form Component

--- p1_ageteller/src/App.js
+++ p1_ageteller/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
+import "./App.css";
 
 const App = () => {
-  return <div>AgeTeller App</div>;
+  return (
+    <div className="App">
+      <Form inline>
+        <h2>Input your Birthday!</h2>
+        <FormControl type="date"></FormControl>
+        {"  "}
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default App;
